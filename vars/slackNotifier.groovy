7f591169a8983308ae9aa0df04230b382dcacb38
Yura Shevelev <sirhikshevelev@gmail.com>
2020-09-08 11:06:14 +0300

    Changes

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String result) {
+def call(@NamedParam(required = true) String result) {
   if ( result == "SUCCESS" ) {
     slackSend message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful", color: "good"
   }
