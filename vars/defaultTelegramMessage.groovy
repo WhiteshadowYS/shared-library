07067fdbd51c85970b47afbba28dc8620747538b
Yura <sirhikshevelev@gmail.com>
2020-09-10 11:02:27 +0300

    Feature

--- vars/defaultTelegramMessage.groovy
+++ vars/defaultTelegramMessage.groovy
@@ -1,7 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String message, String botToken, String chatId) {
-    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+String call(String message, String botToken, String chatId) {
+    print 'Call Start'
+    encodedMessage = URLEncoder.encode(message, 'UTF-8')
 
-    def response = new URL("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage");
+    response = new URL("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage")
+
+    print 'Call before return'
+
+    return response
 }
