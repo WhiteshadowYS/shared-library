3b4eb1aafe0719889956dbb11d1ed612f67aeaf6
Yura <sirhikshevelev@gmail.com>
2020-09-10 12:34:55 +0300

    master

--- vars/defaultTelegramMessage.groovy
+++ vars/defaultTelegramMessage.groovy
@@ -4,7 +4,7 @@ String call(String message, String botToken, String chatId) {
     print 'Call Start'
     encodedMessage = URLEncoder.encode(message, 'UTF-8')
 
-    String url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage";
+    String url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage"
     response = new URL(url).openConnection()
 
     print 'Call before return'
