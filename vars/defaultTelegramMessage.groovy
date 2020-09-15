b5ce360e43bde788290c403aeacd7b5c6442c1b6
Yura Shevelev <sirhikshevelev@gmail.com>
2020-09-15 12:08:32 +0300

    added print of data

--- vars/defaultTelegramMessage.groovy
+++ vars/defaultTelegramMessage.groovy
@@ -3,5 +3,9 @@
 def call(String message, String botToken, String chatId) {
     def encodedMessage = URLEncoder.encode(message, "UTF-8")
 
+    print "message: $encodedMessage"
+    
     def response = new URL("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage").text;
+
+    print "response: $response"
 }
